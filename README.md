b4cb8baabbde25f259526d6720d5aa50f5665904
Adam Howard <skattyadz+gh@gmail.com>
2019-02-12 19:00:29 -0800

    Change name to now-static-build-storybook

--- README.md
+++ README.md
@@ -1,10 +1,10 @@
-# now-static-build-node
+# now-static-build-storybook
 
 This package provides a
 [builder](https://zeit.co/docs/v2/deployments/builders/overview#when-to-use-builders)
-for Zeit's [Now 2.0](https://zeit.co/blog/now-2) that compiles static files with a custom npm script name (not `now-build`)
+for Zeit's [Now 2.0](https://zeit.co/blog/now-2) that compiles [Storybooks](https://github.com/storybooks/storybook)
 
-`now-static-build` always runs the `now-build` script, but we wanted to use the same `package.json` file to deploy a storybook
+`now-static-build` always runs the `now-build` script, but we wanted to reuse our the `package.json` file to deploy a storybook
 
 Based on https://github.com/zeit/now-builders/tree/master/packages/now-static-build
 
@@ -19,12 +19,19 @@ Your `now.json` `"builds"` section should look something like this:
 {
   "builds": [
     {
-      "src": "package.json",
-      "use": "now-static-build-node",
-      "config": {
-        "packageJsonScript": "now-build-storybook"
-      }
+      "src": ".storybook/config.js",
+      "use": "now-static-build-storybook"
     }
   ]
+}
+```
+
+Your `package.json` needs a script named `now-build-storybook`. By default the `dist` directory gets merged with your site, so `dist/storybook` will mount it at `/storybook`
+
+```json
+{
+  "scripts": {
+    "now-build-storybook": "build-storybook -o dist/storybook"
+  }
 }
 ```
--- index.js
+++ index.js
@@ -1,16 +1,15 @@
 const download = require("@now/build-utils/fs/download.js"); // eslint-disable-line import/no-extraneous-dependencies
 const glob = require("@now/build-utils/fs/glob.js"); // eslint-disable-line import/no-extraneous-dependencies
 const path = require("path");
-const { existsSync } = require("fs");
+const { existsSync, readdirSync } = require("fs");
 const {
   runNpmInstall,
-  runPackageJsonScript,
-  runShellScript
+  runPackageJsonScript
 } = require("@now/build-utils/fs/run-user-scripts.js"); // eslint-disable-line import/no-extraneous-dependencies
 
 function validateDistDir(distDir) {
-  const distDirName = path.basename(distDir);
   if (!existsSync(distDir)) {
+    const distDirName = path.basename(distDir);
     const message =
       `Build was unable to create the distDir: ${distDirName}.` +
       "\nMake sure you mentioned the correct dist directory: https://zeit.co/docs/v2/deployments/official-builders/static-build-now-static-build/#configuring-the-dist-directory";
@@ -19,33 +18,27 @@ function validateDistDir(distDir) {
 }
 
 exports.build = async ({ files, entrypoint, workPath, config }) => {
+  if (path.basename(entrypoint) !== "config.js") {
+    throw new Error("Storybook config.js must be specified as entrypoint");
+  }
+
   console.log("downloading user files...");
   await download(files, workPath);
-  console.log("running user scripts...");
-  const mountpoint = path.dirname(entrypoint);
+
+  console.log("running now-build-storybook script...");
+  const mountpoint = path.join(entrypoint, "..", "..");
   const entrypointFsDirname = path.join(workPath, mountpoint);
-  const distPath = path.join(
-    workPath,
-    path.dirname(entrypoint),
-    (config && config.distDir) || "dist"
-  );
 
-  if (!config.packageJsonScript) {
-    throw new Error(
-      `You must provide the package.json script you want to run as config.packageJsonScript`
+  await runNpmInstall(entrypointFsDirname, ["--prefer-offline"]);
+
+  if (await runPackageJsonScript(entrypointFsDirname, "now-build-storybook")) {
+    const distPath = path.join(
+      workPath,
+      mountpoint,
+      (config && config.distDir) || "dist"
     );
+    validateDistDir(distPath);
+    return glob("**", distPath, mountpoint);
   }
-
-  if (path.basename(entrypoint) === "package.json") {
-    await runNpmInstall(entrypointFsDirname, ["--prefer-offline"]);
-    if (
-      await runPackageJsonScript(entrypointFsDirname, config.packageJsonScript)
-    ) {
-      validateDistDir(distPath);
-      return glob("**", distPath, mountpoint);
-    }
-    throw new Error(`An error running "now-build" script in "${entrypoint}"`);
-  }
-
-  throw new Error("Proper build script must be specified as entrypoint");
+  throw new Error(`An error running "now-build" script in "${entrypoint}"`);
 };
